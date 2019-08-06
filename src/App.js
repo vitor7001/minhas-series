a14f92e3d9eb8f7d9b7e08848bcf734bf1a24f9f
tccEtec1 <vitor.pereira124@etec.sp.gov.br>
2019-08-06 14:53:10 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header';
+import Generos from './Generos';
+import NovoGenero from './NovoGenero';
+import EditarGenero from './EditarGenero';
 
+
+import {
+  BrowserRouter as Router,
+  Route
+}  from 'react-router-dom';
+
+const Home = () =>{
+  return <h1> Home </h1>
+}
+
+//node node_modules\minhas-series-server\index.js
 function App() {
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div>
+       <Header />
+       <Route path='/' exact component={Home} />
+       <Route path='/generos' exact component={Generos} />
+       <Route path='/generos/novo' exact component={NovoGenero} />
+       <Route path='/generos/:id' exact component={EditarGenero} />
+       <pre>{JSON.stringify(data)}</pre>
     </div>
+  </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React, { useState} from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+}  from 'reactstrap';
+
+
+import {
+  Link
+}  from 'react-router-dom';
+
+const Header = () =>{
+  const [open, setOpen] = useState(false)
+const toggle = () =>{
+  setOpen(!open)
+}
+
+return (
+     <Navbar color='light' light expand='md'>
+              <NavbarBrand tag={Link} to='/'> Minhas Séries </NavbarBrand>
+              <NavbarToggler onClick={toggle}/>
+                <Collapse  isOpen={open} navbar>
+                   <Nav className='ml-auto'>
+                      <NavItem>
+                        <NavLink tag={Link} to="/generos">
+                          Genêros
+                        </NavLink>
+                      </NavItem>
+                   </Nav>
+                </Collapse>
+     </Navbar>
+  )
+}
+
+export default Header
